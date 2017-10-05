14a205e9e262a35a46552041d9c04fd1a8452fcc
patbit <laburhenk@gmail.com>
2017-10-05 12:23:24 +0200

    improved versions of nuance/bing asr via ws
    removed unnessesary code
    add JSDoc
    disable log messages
    rename folder and id -> all lowcase

--- mmir-plugin-asr-nuancews-web/www/webasrNuanceWsImpl.js
+++ mmir-plugin-asr-nuancews-web/www/webasrNuanceWsImpl.js
@@ -26,20 +26,22 @@
 
 
 /**
- * Media Module: Implementation for Speech Recognition via the Google Web Speech Recognition service v1 using a proxy/mediator server
- * 
- * 
- * @requires Cross-Domain access, if proxy/mediator server is not located in the same domain as the web app
- * @requires CSP for accessing the proxy/mediator server via ws: or wss:, e.g. "connect-src ws://server-address" or "default-src connect-src ws://server-address"
- * 
+ * Media Module: Implementation for Speech Recognition via Bing Service with WebSockets
+ * @author Patrick Bitterling <pabi01@dfki.de>
  */
 newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 	
 
-	/**  @memberOf NuanceWsWebAudioInputImpl# */
+	/**
+	 * @type {string}  
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var MODE = 'nuanceWs';
 
-	/**  @memberOf NuanceWsWebAudioInputImpl# */
+	/**
+	 * @type {string}
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var _pluginName = 'nuanceWsWebAudioInput';
 
 	/** 
@@ -75,43 +77,121 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 	 */
 	var mediaManager = require('mediaManager');
 
-	/** @memberOf NuanceWsWebAudioInputImpl# */
-	var inputId = 0;
 	
-	/** @memberOf NuanceWsWebAudioInputImpl# */
+	/**
+	 * set to true when last recognition result is expected
+	 * @type {boolean}  
+	 * @memberOf NuanceWsWebAudioInputImpl# 
+	 */
 	var lastBlob = false;
 
-	/** @memberOf NuanceWsWebAudioInputImpl# */
+	/**
+	 * if you want to have intermediate results
+	 * @type {boolean}
+	 * @memberOf NuanceWsWebAudioInputImpl# 
+	 **/
 	var isUseIntermediateResults = false;	
 	
-	/** 
+	/**
+	 * the websocket that connects to the service provider 
 	 * @type WebSocket
 	 * @memberOf NuanceWsWebAudioInputImpl#
 	 */
 	var webSocket = null;
 	
+	/** 
+	 * set to true when user presses stop button
+	 * @type {boolean}
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var stopped = true;
 	
-	var textProcessor, currentFailureCallback, closeMicFunc;
 	
-	var WS_STATUS_ENUM = {OPENING : "opening",
-						SETUP : "setup",
-						WORKING : "working",
-						CLOSED : "closed",
-						SEALED : "sealed",
-						NEXT_QUERY : "next_query"
+	/** callback when succesfull recognition happened
+	 * @type {function}
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
+	var textProcessor;
+	
+	
+	/**
+	 * callback when error while recognition happened
+	 * @type {function} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
+	var currentFailureCallback;
+	
+	/**
+	 * function called when the microphone is closed 
+	 * @type {function} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
+	var closeMicFunc;
+	
+	/** 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 * @readonly
+	 * @enum {string}
+	 */
+	var WS_STATUS_ENUM = {OPENING : "opening", // when new WebSocket is called 
+						SETUP : "setup", //after ws.onopen -> send config msg for asr service
+						WORKING : "working", // after setup is completed -> send audio data 
+						CLOSED : "closed", // when error occurs, time out or software stops the connection
+						SEALED : "sealed", // when the user stops the connection
+						NEXT_QUERY : "next_query" // after completing one turn with the asr service
 						};
 	
+	/** 
+	 * Initial status of the websocket
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 * @type enum.WS_STATUS_ENUM
+	 */
 	var wsStatus = WS_STATUS_ENUM.CLOSED;
+	
+	/** 
+	 * contains data that will be saved when ws is not in working state
+	 * @type {Array]
+	 * contains typed array with audio data or JSON message
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var msgBuffer = [];
+	
+	/**
+	 * counts the connection attempts, not handled right now 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 * @type {number}
+	 */
 	var connectionAttempt = 0;
-    var _ttsTransactionId = 0;
+	
+    /** 
+     * id for the transaction
+     * used and incremented at setup nuance 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 * @type {number}
+	 */
     var _asrTransactionId = 1;
-    var _nluTransactionId = 2;
+    
+    /** 
+     * id for the used audio data in the transaction
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 * @type {number}
+	 */
     var _asrRequestId = 0;
     
+    /** 
+     * String loaded from config, representing which encoding should be used
+     * @type {string}
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var encoderStr = configurationManager.getString( [_pluginName, "encoder"] );
 	
+	
+	
+	/** 
+	 * string representing which codec format should be used, opus not implemented yet
+     * @type {string} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var codecTyp = "audio/L16;rate=16000";
 	
 	if(encoderStr.includes("pcm")){
@@ -122,16 +202,37 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 		codecTyp = "audio/opus;rate=16000";
 	}
     
-	
+	/** 
+	 * "true" if recorded data should be send directly to the asr provider -> for now only "true" implemented
+	 * @type {string} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
     var streaming = "true"; //FIXME read from config
     
-    //register listener to get the recorder
+
+    /** 
+     * represents the recorder that encodes the audio data
+     * @type {RecorderExt.class}
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var _recorder;
+	
+	/**
+	 * register listener when audio data is saved, simulates streaming
+	 * @type {function} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var onAudioChunkStored = function(){
 		_recorder.doEncode();
 		_recorder.doFinish();
 	};
 	
+	
+	 /**
+     * register listener to get the recorder
+     * @type {function} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	mediaManager.on('webaudioinputstarted', function(input, audio_context, recorder){
 		_recorder = recorder;
 		var hasListener = recorder.hasListeners("onchunkstored");
@@ -140,7 +241,12 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 		}
 	});
     
-    
+	/** 
+	 * changes the websocket state to the new status, when not in "Sealed"-state
+	 * @type {function}
+	 * @param {WS_STATUS_ENUM} newStatus - change the Websocket to the new status
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
     var changeWsStatus = function(newStatus){
     	//console.log("change wsStatus from "+wsStatus+" to " + newStatus);
     	
@@ -151,9 +257,16 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
     	}
     }
 
-	/** @memberOf NuanceWsWebAudioInputImpl# */
+	/**
+	 * function that sends the data to the asr via websocket
+	 * @type {function}
+	 * @param {typedArray[] | object[typedArray[] | string} } msg - the data to be send
+	 * @param {function} successCallback - the function that shall be called when recognition was succesfull 
+	 * @param {function} failureCallback - the function that shall be called when recognition failed or an error occured
+	 * @memberOf NuanceWsWebAudioInputImpl#  
+	 */
 	var doSend = function(msg, successCallback, failureCallback){
-		//string or object{cmd: 'string', buff:<data>}
+		//string or object{cmd: '<string>', buff:<data>}
 		if(successCallback){
 			textProcessor = successCallback;
 		}
@@ -208,6 +321,11 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 		
 	};
 	
+	/**
+	 * helper function that sends the needed text messages to set up the nuance asr service
+	 * @type {function} 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var setupNuance = function(){
 		connectionAttempt = 0;
 		
@@ -278,7 +396,12 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
             	//changeWsStatus(WS_STATUS_ENUM.WORKING);
             }
 	}
-
+	
+	/** 
+	 * helper function that sends all bufferd messages to the asr
+	 * @type {function}
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var sendMsgBuffer = function(){
 		var audioEndFlag = false;
 		
@@ -313,13 +436,12 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 	
 	/** initializes the connection to the Nuance-server, 
 	 * where the audio will be sent in order to be recognized.
-	 * 
+	 * @type {function}
 	 * @memberOf NuanceWsWebAudioInputImpl#
 	 */
-	var buildConnection = function(oninit){ 
+	var buildConnection = function(/*oninit*/){//TODO check if this does work else add JSDoc param 
 		if(wsStatus == WS_STATUS_ENUM.OPENING || wsStatus == WS_STATUS_ENUM.SETUP || wsStatus == WS_STATUS_ENUM.WORKING || wsStatus == WS_STATUS_ENUM.SEALED ){
-		//if(!(wsStatus == WS_STATUS_ENUM.NEXT_QUERY || wsStatus == WS_STATUS_ENUM.CLOSED)){
-			//console.log("canceled buildConnection: " + wsStatus);
+		//early exit if function is called when connection building is in progress
 			return;
 		}
 		
@@ -334,10 +456,13 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 		webSocket = new WebSocket(serviceUrl);
 		webSocket.binaryType = 'arraybuffer';
 
-		/**  @memberOf NuanceWsWebAudioInputImpl.webSocket# */
+		/**
+		 * function called when websocket connects with the asr service  
+		 * @type {function}
+		 * @memberOf NuanceWsWebAudioInputImpl.webSocket# */
 		webSocket.onopen = function () {
 			
-			//PatBit TODO use another method for unique id
+			//PatBit TODO maybe use another method for unique id
 	        var nav = window.navigator;
 	           var deviceId = [
 	               nav.platform,
@@ -352,20 +477,21 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 	            'device_id': deviceId
 	        };
 			
-			//console.log("buildconnect send");
 			webSocket.send(JSON.stringify(_connect));
 			setupNuance();
 		};
-		/**  @memberOf NuanceWsWebAudioInputImpl.webSocket# */
+		/**
+		 * function that is called when the websocket receives a message
+		 * @type {function}
+		 * @memberOf NuanceWsWebAudioInputImpl.webSocket#
+		 */
 		webSocket.onmessage = function(msg) {
 			
 			var msg = JSON.parse(msg.data);
 			var cmd = msg.message;
-			//console.log("debug info: " + JSON.stringify(msg));
             
 			switch(cmd){
 				case 'connected':
-					//console.log("connected sendMsgBuffer");
 					sendMsgBuffer();
 					changeWsStatus(WS_STATUS_ENUM.WORKING);
 					return;
@@ -414,7 +540,11 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 			console.warn("unhandled webSocket.onmessage"); 
             
 		};
-		/**  @memberOf NuanceWsWebAudioInputImpl.webSocket# */
+		/** function that is called when the websocket connection is faulty
+		 *  @param {object} e - an error object created by the browser
+		 *  @type {function}
+		 *  @memberOf NuanceWsWebAudioInputImpl.webSocket#
+		 */
 		webSocket.onerror = function(e) {
 			//console.log("ws.onerror called");
 			websocket = null;
@@ -448,38 +578,41 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 				console.error('Websocket Error: '+e  + (e.code? ' CODE: '+e.code : '')+(e.reason? ' REASON: '+e.reason : ''));
 			}
 		};
-		/**  @memberOf NuanceWsWebAudioInputImpl.webSocket# */
+		/** 
+		 *  function that is called when the websocket is closed 
+		 *	@type {function}
+		 *  @param {object} e - an error object
+		 *	@memberOf NuanceWsWebAudioInputImpl.webSocket#
+		 */
 		webSocket.onclose = function(e) {
-			console.info('Websocket closed!'+(e.code? ' CODE: '+e.code : '')+(e.reason? ' REASON: '+e.reason : ''));
+			//console.info('Websocket closed!'+(e.code? ' CODE: '+e.code : '')+(e.reason? ' REASON: '+e.reason : ''));
 			websocket = null;
 		};
 	};
 	
-	/** @memberOf NuanceWsWebAudioInputImpl# */
-	var buffer = 0;
-	
-	/** @memberOf NuanceWsWebAudioInputImpl# */
+	/** 
+	 * the function that is called when audio data must be send because the data amount gets to large in size
+	 * the function is called from the silence detection
+	 * @type {function}
+	 * @param {object} evt - event object that includes the current recorder
+	 * @returns {boolean} is always false
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var onSendPart = function(evt){		
 			var recorder = evt.recorder;
 			
 			recorder.doEncode();
 			recorder.doFinish();
 
-	
-			
-//			recorder && recorder.exportMonoPCM(
-//					/** @memberOf NuanceWsWebAudioInputImpl.recorder# */
-//					function onSendPartial(view){ //blob, id
-//							doSend(view.buffer);
-//					},
-//					buffer,
-//					inputId
-//			);
-
 		return false;
 	};
 
-	/** @memberOf NuanceWsWebAudioInputImpl# */
+	/**
+	 * function that is called when the silence detection registers silence
+	 * @type {function}
+	 * @param {object} evt - event object that includes the current recorder
+	 * @returns {boolean} is always false
+	 * @memberOf NuanceWsWebAudioInputImpl# */
 	var onSilence = function(evt){
 
 		var recorder = evt.recorder;
@@ -495,31 +628,8 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 	                'audio_id': _asrRequestId
 	            };
 			
-			doSend(JSON.stringify(_audio_end));
-			
-			
-			// send record to server!
-			//console.log("onSilence NOT STREAMING !!!");
-			//recorder && recorder.exportMonoPCM(
-			//		/** @memberOf NuanceWsWebAudioInputImpl.recorder# */
-			//		function onSilenceDetected(view){ //blob,id
-			//			console.log("onSilenceDetected entered Method");
-			//			console.log("buffersize: " + view.buffer.byteLength);
-			//			if(mediaManager._log.isDebug()) mediaManager._log.log("monoPCM exported");
-			//				doSend(view.buffer);
-			//				
-			//				//TODO make code cleaner
-			//				var _audio_end = {
-			//		                'message': 'audio_end',
-			//		                'audio_id': _asrRequestId
-			//		            };
-			//				
-			//		        doSend(JSON.stringify(_audio_end));
-			//				hasActiveId = false;
-			//		},
-			//		buffer,
-			//		inputId
-			//);
+			doSend(JSON.stringify(_audio_end));			
+
 		}else{ //we are streaming
 			//TODO make code cleaner
 			var _audio_end = {
@@ -535,13 +645,26 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 		return false;
 	};
 
-	/** @memberOf NuanceWsWebAudioInputImpl# */
+	/** 
+	 * function is called by the silence detection when there is no noise before speaking
+	 * it clears the buffered audio data that contains to reduce the encoded and send data
+	 * @type {function}
+	 * @param {object} evt - event object that includes the current recorder
+	 * @returns {boolean} is always false 
+	 * @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var onClear = function(evt){
 
 		evt.recorder && evt.recorder.clear();
 		return false;
 	};
 	
+	/** 
+	 *  function puts websocket in seal status, which can't be changed until the user starts the recording again
+	 *  sends message to nuance ending the current turn
+	 *  @type {function}
+	 *  @memberOf NuanceWsWebAudioInputImpl#
+	 */
 	var sealWebsocket = function(){
 		
 		if(wsStatus == WS_STATUS_ENUM.WORKING){
@@ -554,16 +677,26 @@ newWebAudioAsrImpl = (function NuanceWsWebAudioInputImpl() {
 		changeWsStatus(WS_STATUS_ENUM.SEALED);
 	}
 
+	/**
+	 * function that calls buildConnection and returns false  
+	 * @type {function}
+	 * @returns {boolean} is always false 
+	 * @memberOf NuanceWsWebAudioInputImpl# 
+	 */
 	var buildConnectionWrapper = function(){
 		buildConnection();
 		return false;
 	}
 	
+	/** function that returns false for the oninit call
+	 * @type {function}
+	 * @returns {boolean} is always false 
+	 * @memberOf NuanceWsWebAudioInputImpl# 
+	 */
 	var pseudoInit = function(){
-		//console.log("pseudoInit called teste");
 		return false;
 	}
-	/**  @memberOf NuanceWsWebAudioInputImpl# */
+	
 	return {
 		/** @memberOf NuanceWsWebAudioInputImpl.AudioProcessor# */
 		_init: pseudoInit,
